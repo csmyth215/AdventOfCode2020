a034f33b033a2769f5a713b6e755c9e0f8d62079
Chloe Smyth <chloe.smyth57@gmail.com>
2020-12-28 15:54:55 +0000

    Solve Puzzle Day 15

---
+++ Day_Fifteen.py
@@ -0,0 +1,27 @@
+puzzle_input = [5,1,9,18,13,8,0]
+
+def save_part_one_output(puzzle_data):
+    new_f = open('puzzle_inputs/Day_Fifteen_Part_One.txt', "w")
+    for n in range(0, (max(puzzle_data) + 1)):
+        my_string = f"There are {puzzle_data.count(n)} instances of {n} spoken.\n"
+        new_f.write(my_string)
+    new_f.close()
+
+last_occurrences = {}
+for i in puzzle_input[:-1]:
+    last_occurrences[i] = puzzle_input.index(i)
+
+last_position = len(puzzle_input) - 1
+i = puzzle_input[-1]
+while last_position < (30000000 - 1):
+    if i not in last_occurrences.keys():
+        last_occurrences[i] = last_position
+        i = 0
+    else:
+        turns_apart = last_position - last_occurrences[i]
+        last_occurrences[i] = last_position
+        i = turns_apart
+
+    last_position += 1
+
+print(i)
